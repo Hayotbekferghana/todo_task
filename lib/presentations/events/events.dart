7ff1ae31969ac64586d40dde71a3eba2817801d8
Hayotbek <ferghanskiy@gmail.com>
2023-08-06 12:50:32 +0500

    initial commit

---
+++ lib/presentations/events/events.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class EventsScreen extends StatelessWidget {
+  const EventsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) =>
+      const Scaffold(body: Center(child: Text("Events")));
+}
